d4c64de245562c1b9efe385f8d13957eafbb5e05
S29Ducky <163264380+S29Ducky@users.noreply.github.com>
2025-05-07 10:26:36 +0200

    fix(token): hide

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 const UrlApi = "https://api.github.com/users/";
-const token =
+const SC =
 	"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";
-const InputSearchUser = document.querySelector("#InputSearchUser")
-const BtnValide = document.querySelector("#BtnValide")
-const UserResultCase = document.querySelector("#UserResultCase")
-const PpUser = document.querySelector("#PpUser")
-const loginUser = document.querySelector("#name")
-const username = document.querySelector("#usernam")
-const joined = document.querySelector("#joined")
-const bio = document.querySelector("#bio")
+const InputSearchUser = document.querySelector("#InputSearchUser");
+const BtnValide = document.querySelector("#BtnValide");
+const UserResultCase = document.querySelector("#UserResultCase");
+const PpUser = document.querySelector("#PpUser");
+const loginUser = document.querySelector("#name");
+const username = document.querySelector("#usernam");
+const joined = document.querySelector("#joined");
+const bio = document.querySelector("#bio");
 
-BtnValide.addEventListener("click", getUser)
-
+BtnValide.addEventListener("click", getUser);
 
 async function getUser() {
 	const response = await fetch(UrlApi + "S29Ducky", {
 		headers: {
-			Authorization: `token ${token}`,
+			Authorization: `Bearer ${SC}`,
 		},
 	});
 
@@ -26,12 +25,11 @@ async function getUser() {
 	let loginData = data.login;
 	let UsernameData = data.name;
 	let dateCreationData = data.created_at;
-    let BioData = data.bio
+	let BioData = data.bio;
 
-    PpUser.textContent = UserPpData
-    loginUser.textContent = loginData
-    username.textContent = UsernameData
-    joined.textContent = dateCreationData
-    bio.textContent = BioData
-    
+	PpUser.textContent = UserPpData;
+	loginUser.textContent = loginData;
+	username.textContent = UsernameData;
+	joined.textContent = dateCreationData;
+	bio.textContent = BioData;
 }
